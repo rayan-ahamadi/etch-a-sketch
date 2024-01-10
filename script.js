c65dad9861f0f4d8f758da9a9bd2f309039a37bc
rayan-ahamadi <rayan.ahamadi@laplateforme.io>
2024-01-10 15:47:30 +0100

    tenter une résolution au ralentissement de la page
    ralentissement de la page lors de changement de dimension supérieur à
    48x48. Donc les dimensions de l'écran de dessin ne dépasseront pas ce seuil.

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 const grid = document.querySelector('#grid');
+const resizeCursor = document.querySelector('#resize-cursor');
+const cursorLabel = document.querySelector('#cursor-label');
 
 function fillGrid(dimension){
-    while(grid.firstChild){
-        grid.removeChild(grid.firstChild);
-    }
-    for(let i = 1; i < dimension; i++){
+    for(let i = 0; i < dimension; i++){
         let newDivRow = document.createElement('div');
         newDivRow.classList.add('grid-row');
         grid.appendChild(newDivRow);
         for(let j = 1; j < dimension; j++){
             let newDivColumn = document.createElement('div');
-            newDivColumn.classList.add('grid-cell');
-            newDivColumn.style['border'] = '1px solid black';
             newDivColumn.style['height'] = `${(grid.offsetHeight / dimension) - 2}px`;
+            newDivColumn.classList.add('grid-cell');
             newDivRow.appendChild(newDivColumn);
         }
     }
 }
+
+
+resizeCursor.addEventListener('change', () => {
+    while(grid.firstChild){
+        grid.removeChild(grid.firstChild);
+    }
+    let value = resizeCursor.value;
+    fillGrid(parseInt(value));
+    cursorLabel.textContent = `${value}x${value}`;
+});
 
 fillGrid(16);
