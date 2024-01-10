9524bae7b5b7400655eb771d748c00388eddff1d
rayan-ahamadi <rayan.ahamadi@laplateforme.io>
2024-01-10 16:28:04 +0100

    ajouter la logique de l'écran magique pour dessiner

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const grid = document.querySelector('#grid');
 const resizeCursor = document.querySelector('#resize-cursor');
 const cursorLabel = document.querySelector('#cursor-label');
+const clearButton = document.querySelector('#clear');
 
 function fillGrid(dimension){
     for(let i = 0; i < dimension; i++){
@@ -9,12 +10,32 @@ function fillGrid(dimension){
         grid.appendChild(newDivRow);
         for(let j = 1; j < dimension; j++){
             let newDivColumn = document.createElement('div');
+            newDivColumn.style['border'] = '0.1px solid black';
             newDivColumn.style['height'] = `${(grid.offsetHeight / dimension) - 2}px`;
             newDivColumn.classList.add('grid-cell');
+            grid.addEventListener('mousedown', () => {
+                newDivColumn.addEventListener('mouseover', changeColor); 
+            });
+            grid.addEventListener('mouseup', () => {
+                newDivColumn.removeEventListener('mouseover', changeColor);
+            });
             newDivRow.appendChild(newDivColumn);
         }
     }
 }
+
+function changeColor(e){
+    if(e.target.classList.contains('grid-cell')){
+        e.target.style['background-color'] = 'black';
+    }
+}
+
+clearButton.addEventListener('click', () => {
+    let cells = document.querySelectorAll('.grid-cell');
+    cells.forEach(cell => {
+        cell.style['background-color'] = 'cadetblue';
+    });
+})
 
 
 resizeCursor.addEventListener('change', () => {
